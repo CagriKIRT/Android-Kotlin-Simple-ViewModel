74741ebe7db71690764e90279b3046f4840742db
ÇağrıKIRT <cagrikirt@gmail.com>
2021-03-12 18:53:30 +0300

    Implement button textview and viewmodel to the MainActivity

--- app/src/main/java/com/example/viewmodelsample/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelsample/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.viewmodelsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView:TextView
+    lateinit var plusOneButton:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView=findViewById(R.id.textView)
+        plusOneButton=findViewById(R.id.plusOneButton)
+
+        val viewModel=ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        textView.text=viewModel.number.toString()
+
+        plusOneButton.setOnClickListener {
+
+            viewModel.addNumber()
+            textView.text=viewModel.number.toString()
+        }
+
     }
 }
